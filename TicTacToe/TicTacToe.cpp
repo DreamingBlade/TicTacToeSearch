4155e9fbe7bd6bc25ee4174c61a0e4a4766809ec
Keegan Buckingham <keegan.buckingham@mymail.champlain.edu>
2024-04-16 10:42:06 -0400

    Setting up the win states.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -29,9 +29,9 @@ int main()
 
     //Putting out the board.
     cout << "    1   2   3\n"
-        "1   " << board[0][0].mark << " | " << board[0][1].mark << " | " << board[0][2].mark << "\n    _________\n" <<
-        "2   " << board[1][0].mark << " | " << board[1][1].mark << " | " << board[1][2].mark << "\n    _________\n" <<
-        "3   " << board[2][0].mark << " | " << board[2][1].mark << " | " << board[2][2].mark << "\n";
+        "1   " << board[0][0].mark << " | " << board[1][0].mark << " | " << board[2][0].mark << "\n    _________\n" <<
+        "2   " << board[0][1].mark << " | " << board[1][1].mark << " | " << board[2][1].mark << "\n    _________\n" <<
+        "3   " << board[0][2].mark << " | " << board[1][2].mark << " | " << board[2][2].mark << "\n";
 
     while (gameRunning)
     {
@@ -80,12 +80,92 @@ int main()
             }
         }
         valid = false;
+        //Putting out the board.
         cout << "    1   2   3\n"
-            "1   " << board[0][0].mark << " | " << board[0][1].mark << " | " << board[0][2].mark << "\n    _________\n" <<
-            "2   " << board[1][0].mark << " | " << board[1][1].mark << " | " << board[1][2].mark << "\n    _________\n" <<
-            "3   " << board[2][0].mark << " | " << board[2][1].mark << " | " << board[2][2].mark << "\n";
+            "1   " << board[0][0].mark << " | " << board[1][0].mark << " | " << board[2][0].mark << "\n    _________\n" <<
+            "2   " << board[0][1].mark << " | " << board[1][1].mark << " | " << board[2][1].mark << "\n    _________\n" <<
+            "3   " << board[0][2].mark << " | " << board[1][2].mark << " | " << board[2][2].mark << "\n";
 
+        bool Owin = false;
+        bool Xwin = false;
+        //Checking if O won.
+        if (board[0][0].mark == 'O' && board[0][1].mark == 'O' && board[0][2].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[1][0].mark == 'O' && board[1][1].mark == 'O' && board[1][2].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[2][0].mark == 'O' && board[2][1].mark == 'O' && board[2][2].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[0][0].mark == 'O' && board[1][1].mark == 'O' && board[1][2].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[2][0].mark == 'O' && board[1][1].mark == 'O' && board[0][2].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[0][0].mark == 'O' && board[1][0].mark == 'O' && board[2][0].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[0][1].mark == 'O' && board[1][1].mark == 'O' && board[2][1].mark == 'O')
+        {
+            Owin = true;
+        }
+        else if (board[0][2].mark == 'O' && board[1][2].mark == 'O' && board[2][2].mark == 'O')
+        {
+            Owin = true;
+        }
 
+        //Checking if X wins
+        if (board[0][0].mark == 'X' && board[0][1].mark == 'X' && board[0][2].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[1][0].mark == 'X' && board[1][1].mark == 'X' && board[1][2].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[2][0].mark == 'X' && board[2][1].mark == 'X' && board[2][2].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[0][0].mark == 'X' && board[1][1].mark == 'X' && board[1][2].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[2][0].mark == 'X' && board[1][1].mark == 'X' && board[0][2].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[0][0].mark == 'X' && board[1][0].mark == 'X' && board[2][0].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[0][1].mark == 'X' && board[1][1].mark == 'X' && board[2][1].mark == 'X')
+        {
+            Xwin = true;
+        }
+        else if (board[0][2].mark == 'X' && board[1][2].mark == 'X' && board[2][2].mark == 'X')
+        {
+            Xwin = true;
+        }
 
+        if (Xwin == true)
+        {
+            cout << "\n\n THE X PLAYER WINS\n\n";
+            gameRunning = false;
+        }
+
+        if (Owin == true)
+        {
+            cout << "\n\n THE O PLAYER WINS\n\n";
+            gameRunning = false;
+        }
     }
 }
